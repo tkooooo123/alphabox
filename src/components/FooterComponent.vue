30add01446f2598905b258ca33a48917f28cad42
tkooooo123 <tkooooo123@gmail.com>
2023-09-10 16:57:54 +0800

    調整 Footer 排版

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,16 +1,16 @@
 <template>
     <footer>
-    <div class="container lg:flex items-center justify-between bg-white text-black lg:max-w-[1296px] lg:py-12 py-10">
-      <div>
+    <div class="container lg:grid grid-cols-3 bg-white text-black lg:max-w-[1296px] lg:py-12 py-10">
+      <div class="col-span-1">
         <h3 class="text-8 font-shrik">ALPHABOX+</h3>
         <p class="font-bold tracking-widest hidden lg:block">Copyright © 2023 Hexschool.</p>
       </div>
-      <div class="mt-2 lg:mt-0">
+      <div class="mt-2 lg:mt-0 col-span-1 text-center">
         <span class="material-symbols-outlined text-[80px]">
           emergency
         </span>
       </div>
-      <ul class="lg:flex items-center gap:2 lg:gap-6 font-bold ">
+      <ul class="col-span-1 lg:flex items-center justify-end gap:2 lg:gap-6 font-bold ">
         <li class="mt-2">關於我們</li>
         <li class="mt-2">產品資訊</li>
         <li class="mt-2">常見問題</li>
